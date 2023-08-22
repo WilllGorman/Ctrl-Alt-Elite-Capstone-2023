261799193f8764f1d08757dbb8cc01653af1025d
Will Gorman <n10996818@qut.edu.au>
2023-08-23 09:29:19 +1000

    Comments to deployment_service.ts

--- react-cdk-test/infra/lib/deployment_service.ts
+++ react-cdk-test/infra/lib/deployment_service.ts
@@ -5,24 +5,30 @@ import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
+// Defines project output
 const path = './resources/build';
 
+// extends Construct is best practice for creating stack
 export class DeploymentService extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
+        // Create S3 Bucket to hold React application
         const hostingBucket = new Bucket(this, "FrontendBucket", {
             autoDeleteObjects: true,
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
             removalPolicy: RemovalPolicy.DESTROY,
         });
-
+        
+        // 
         const distribution = new Distribution(this, 'CloudfrontDistribution', {
             defaultBehavior: {
                 origin: new S3Origin(hostingBucket),
                 viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
             },
+            // Entry point of application
             defaultRootObject: "index.html",
+            // Error repsonses for accessing the Cloudfront distribution
             errorResponses: [
                 {
                     httpStatus: 404,
@@ -32,19 +38,23 @@ export class DeploymentService extends Construct {
             ],
         });
 
+        // Deploys solution to bucket
+        // Copies project over to bucket, and clears the CDN cache - only new project is pulled.
         new BucketDeployment(this, 'BucketDeployment', {
             sources: [Source.asset(path)],
             destinationBucket: hostingBucket,
             distribution,
-            distributionPaths: ["/*"],
+            distributionPaths: ["/*"], // Tells CDK to clear the CDN cache to ensure newest project is pulled
         });
 
+        // Outputs to generate link for URL
         new CfnOutput(this, 'CloudFrontURL', {
             value: distribution.domainName,
             description: "This Distribution URL",
             exportName: "CloudfrontURL",
         });
-
+        
+        // Output to generate Bucket name in console
         new CfnOutput(this, "BucketName", {
             value: hostingBucket.bucketName,
             description: "The name of the S3 bucket",
